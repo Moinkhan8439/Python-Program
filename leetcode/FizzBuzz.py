323a9bd20cc1f097cbab1d88ca67803475fcf40c
Moin <moinkhan8439@gmail.com>
2022-10-17 11:39:32 +0530

    17-Oct-22 morning

--- leetcode/FizzBuzz.py
+++ leetcode/FizzBuzz.py
@@ -26,7 +26,7 @@ Constraints:
 '''
 
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+    def fizzBuzz(self, n: int) -> list[str]:
         return ["Fizz"*(d%3==0)+"Buzz"*(d%5==0) or f"{d}" for d in range(1,n+1)]
 
 s=int(input())
