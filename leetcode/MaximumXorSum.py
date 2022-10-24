279f20b00f7c564608976ee3982ba129b356760e
Moin <moinkhan8439@gmail.com>
2022-10-24 18:44:04 +0530

    Happy Diwali

---
+++ leetcode/MaximumXorSum.py
@@ -0,0 +1,41 @@
+'''
+421. Maximum XOR of Two Numbers in an Array
+Medium
+
+4497
+
+343
+
+Add to List
+
+Share
+Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
+
+ 
+
+Example 1:
+
+Input: nums = [3,10,5,25,2,8]
+Output: 28
+Explanation: The maximum result is 5 XOR 25 = 28.
+Example 2:
+
+Input: nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+Output: 127
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 105
+0 <= nums[i] <= 231 - 1
+'''
+
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        for i in nums:
+            for j in nums:  
+                pass
+
+l=list(map(int,input().split()))
+print(Solution.findMaximumXOR(Solution,l))
